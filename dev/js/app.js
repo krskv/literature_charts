8f75a53819ede77cf0f315a840335f65258f61c5
yuriy <yuriy@mugsoft.io>
2018-12-08 14:23:21 +0400

    Добавил возможность делиться выделенным текстом в телеграмм и по почте в разделе форматируемого текста. Изменения в main.css, общей шапке и app.js

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,9 +1,9 @@
 (function() {
 
    $(window).scroll(function() {
-		var win = $(window);
-		var main_header = $('.main_header');
-		var is_scroll_distance = win.scrollTop() > ( win.height() * 0.8 );
+		var win                 = $(window);
+		var main_header         = $('.main_header');
+		var is_scroll_distance  = win.scrollTop() > ( win.height() * 0.8 );
 
     is_scroll_distance && main_header ? main_header.addClass('minified') : main_header.removeClass('minified');
 
@@ -12,43 +12,86 @@
   $(window).resize(function() {
   });
 
+  //WOW PLUGIN INITIALIZATION
   new WOW().init();
+  //==========
 
-function setVideoHeight() {
-  var a = $( '#main-video-box' );
-  var b = $( '#video-modal .video-wrapper' );
-  var c = b.width() * 0.56;
+  //HAMBURGER TOGGLE
 
-  a.height(c);
-  b.height(c);
-}
+  $(".hamburger").click(function(){
+    $(this).toggleClass("is_active");
+    $('header').toggleClass("expanded");
+  });
 
+  //==========
 
+  //HIGHLIGHTED TEXT SHARING
 
-//Hamburger toggle
+  //Activate highlighted text sharing
+  $('.formated_text').mouseup(function(){
+    setTimeout( textHighlightSharing, 100 );
+  });
 
-$(".hamburger").click(function(){
-	$(this).toggleClass("is_active");
-	$('header').toggleClass("expanded");
-});
+  function textHighlightSharing(){
+    var share       = $('#selection_sharing'),
+        highlight   = getHighlight();
 
-//=====
+    if (highlight.text === ''){
+      share.removeClass('visible');
+      return;
+    }
 
+    var url         = window.location.href;
+    var top_margin  = $('body').css('marginTop').replace( 'px', '');
+
+    share.css('top', highlight.offset.top - top_margin - 50 + 'px')
+    share.css('left', highlight.offset.left +'px');
+    share.addClass('visible');
+
+    $('.selection_sharing__twitter').attr('href','https://twitter.com/intent/tweet?text='+ encodeURIComponent(txt) + encodeURIComponent('\n') +'&url=' + encodeURIComponent(url));
+    $('.selection_sharing__mail').attr('href','mailto:?subject=' + encodeURIComponent(txt) + '&body=' + encodeURIComponent(txt) + encodeURIComponent('\n') + encodeURIComponent(url));
+
+  };
+
+  function getHighlight() {
+
+    var selection = window.getSelection(),
+        object = {
+          text   : '',
+          offset : null
+        };
+
+    if ( selection.rangeCount > 0 ) {
+        var rect        = selection.getRangeAt(0).getBoundingClientRect(),
+            scrollLeft  = window.pageXOffset ||document.documentElement.scrollLeft,
+            scrollTop   = window.pageYOffset || document.documentElement.scrollTop;
+        object = {
+            text   : selection.toString().trim(),
+            offset   : { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+        };
+    }
+
+    return object;
+  }
+
+  //==========
 })(jQuery);
 
+//SMOOTHSCROLL ACTIVATION
 var scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000, // Integer. How fast to complete the scroll in milliseconds
   easing: 'easeInOutCubic' // Easing pattern to use
 });
+//==========
 
+//ACTIVATE ONLOAD ANIMATION
 window.onload = function () {
-
-  var loader_screen = document.querySelector('#loader_screen');
-  var main_header =  document.querySelector('.main_header');
+  var loader_screen   = document.querySelector('#loader_screen');
+  var main_header     =  document.querySelector('.main_header');
 
   if (loader_screen)
     loader_screen.className = 'ontop'
   if (main_header)
     main_header.className += ' animated';
-
 }
+//==========
